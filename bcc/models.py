65af86414a6ecfdc2e91013a0af5d995e38acce5
rizzu1023 <mrizwan9022@gmail.com>
2018-12-04 01:03:41 +0530

    nav-bar

--- bcc/models.py
+++ bcc/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    user_id = models.IntegerField()
+    password = models.CharField(max_length=20)   #length parameter is compulsory
+
+    def __str__(self):
+        return self.password          #can't return a integer value
+
+
